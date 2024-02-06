1d041ba4763d45a90696c908f6145448c4c01185
asharien <asharien@naver.com>
2024-02-06 20:05:24 +0900

    Upload from Termux Tue Feb  6 20:05:24 KST 2024

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 git init
+git remote remove main
+git remote set-url origin https://github.com/asharien/BOJ_ProblemSolve13000-14000.git
 git remote add origin https://github.com/asharien/BOJ_ProblemSolve13000-14000.git
+git branch -m origin main
+git branch
 git remote -v
 git status
 git log
